3635ee8231fd522cbd6aebb0de5cd329bc935921
Giles-Billenness <giles.billenness@blueyonder.co.uk>
2024-08-07 18:08:24 +0100

    add larger img size scripts

---
+++ customNext512.sh
@@ -0,0 +1,20 @@
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=48798 main_finetune.py \
+    --batch_size 3 \
+    --world_size 1 \
+    --model convnextv2_large \
+    --epochs 50 \
+    --blr 5e-3 --layer_decay 0.65 \
+    --weight_decay 0.05 --drop_path 0.2 \
+    --nb_classes 5 \
+    --data_path ../Data/Kaggle/augmented_resized_V2/ \
+    --task CustomConvnext \
+    --log_dir ../Data/Results/CustomConvnext/ \
+    --output_dir ../Data/Results/ \
+    --finetune ../Data/RETFound_cfp_weights.pth \
+    --clip_grad 0.8 \
+    --input_size 512
+
+# 40 -> 21481
+# 44 -> 23137* this one
+
+    # --clip_grad 1 \
---
+++ customViT512.sh
@@ -0,0 +1,25 @@
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=48798 main_finetune.py \
+    --batch_size 3 \
+    --world_size 1 \
+    --model vit_large_patch16 \
+    --epochs 50 \
+    --blr 5e-3 --layer_decay 0.65 \
+    --weight_decay 0.05 --drop_path 0.2 \
+    --nb_classes 5 \
+    --data_path ../Data/Kaggle/augmented_resized_V2/ \
+    --task CustomRETFound512 \
+    --log_dir ../Data/Results/CustomRETFound512/ \
+    --output_dir ../Data/Results/ \
+    --finetune ../Data/RETFound_cfp_weights.pth \
+    --input_size 512
+
+# default
+# --batch_size 32 \
+# --input_size 224
+
+# large 512
+# --batch_size 3 \
+# --input_size 512
+
+# 40 -> 21481
+# 44 -> 23137* this one
